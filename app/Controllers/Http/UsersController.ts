1252a60e7145e517f30d3a8b591f0d3f8eed9069
eskokado <eskokado@gmail.com>
2024-01-23 16:08:51 -0300

    feat: throw exception when email already in use

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,11 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequest from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
     const userByEmail = await User.findBy('email', userPayload.email)
-    if (userByEmail) return response.conflict({ message: 'email already in use' })
+    if (userByEmail) throw new BadRequest('email already in use', 409)
     const user = await User.create(userPayload)
     return response.created({ user })
   }
