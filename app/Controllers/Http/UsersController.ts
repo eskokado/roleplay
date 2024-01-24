2222c1635faeb60759c1d91e41aa535a45d934c8
eskokado <eskokado@gmail.com>
2024-01-24 12:28:04 -0300

    feat: it should return 422 when required data is not provided

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequest from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 import CreateUserValidator from 'App/Validators/CreateUserValidator'
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
@@ -18,13 +19,13 @@ export default class UsersController {
   }
 
   public async update({ request, response }: HttpContextContract) {
-    const { email, password, avatar } = request.only(['email', 'avatar', 'password'])
+    const userPayload = await request.validate(UpdateUserValidator)
     const id = request.param('id')
 
     const user = await User.findOrFail(id)
-    user.email = email
-    if (avatar) user.avatar = avatar
-    if (password) user.password = password
+    user.email = userPayload.email
+    if (userPayload.avatar) user.avatar = userPayload.avatar
+    if (userPayload.password) user.password = userPayload.password
     await user.save()
 
     return response.ok({ user })
