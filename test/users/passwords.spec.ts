70df06f35b869df1fae5014c559d594126eef5e0
eskokado <eskokado@gmail.com>
2024-01-27 15:30:01 -0300

    test: it cannot reset password when token is expired after 2 hours

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -4,6 +4,7 @@ import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
+import { DateTime, Duration } from 'luxon'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -93,6 +94,21 @@ test.group('Password', (group) => {
 
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 404)
+  })
+
+  test('it cannot reset password when token is expired after 2 hours', async (assert) => {
+    const user = await UserFactory.create()
+    const date = DateTime.now().minus(Duration.fromISOTime('02:01'))
+    const { token } = await user.related('tokens').create({ token: 'token', createdAt: date })
+
+    const { body } = await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(410)
+
+    assert.equal(body.code, 'TOKEN_EXPIRED')
+    assert.equal(body.status, 410)
+    assert.equal(body.message, 'token has expired')
   })
 
   group.beforeEach(async () => {
