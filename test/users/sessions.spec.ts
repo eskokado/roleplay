f0c0fb5de1847896143e7cfbb141d648bddd88c2
eskokado <eskokado@gmail.com>
2024-01-28 09:52:37 -0300

    test: it should return an api when session is created

--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -20,6 +20,31 @@ test.group('Session', (group) => {
     assert.equal(body.user.id, id)
   })
 
+  /*
+  {
+    user: {
+
+    },
+    token: {
+
+    }
+  }
+  */
+
+  test('it should return an api when session is created', async (assert) => {
+    const plainPassword = 'test'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    console.log({ user: body.user })
+
+    assert.isDefined(body.token, 'Token undefined')
+    assert.equal(body.user.id, id)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
