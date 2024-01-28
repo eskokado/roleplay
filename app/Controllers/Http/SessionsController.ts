ca27eb8683b74af5ec40aaad6fa059087317dc20
eskokado <eskokado@gmail.com>
2024-01-28 09:52:26 -0300

    feat: it should return an api when session is created

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -3,8 +3,10 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
-    await auth.attempt(email, password)
+    const token = await auth.use('api').attempt(email, password, {
+      expiresIn: '2hours',
+    })
 
-    return response.created({ user: auth.user })
+    return response.created({ user: auth.user, token })
   }
 }
