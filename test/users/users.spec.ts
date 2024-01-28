8e50c3e2a82568f3dc3b54cc247501611416eaa3
eskokado <eskokado@gmail.com>
2024-01-28 12:00:19 -0300

    test: add identify user

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,5 +1,6 @@
 import Hash from '@ioc:Adonis/Core/Hash'
 import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
@@ -18,6 +19,7 @@ const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 */
 
 let token = ''
+let user = {} as User
 
 test.group('User', (group) => {
   test('it should create an user', async (assert) => {
@@ -86,24 +88,22 @@ test.group('User', (group) => {
   })
 
   test('it should update an user', async (assert) => {
-    const { id, password } = await UserFactory.create()
     const email = 'test@test.com'
     const avatar = 'http://github.com/giuliana-bezerra.png'
 
     const { body } = await supertest(BASE_URL)
-      .put(`/users/${id}`)
+      .put(`/users/${user.id}`)
       .set('Authorization', `Bearer ${token}`)
-      .send({ email, avatar, password })
+      .send({ email, avatar, password: user.password })
       .expect(200)
 
     assert.exists(body.user)
     assert.equal(body.user.email, email)
     assert.equal(body.user.avatar, avatar)
-    assert.equal(body.user.id, id)
+    assert.equal(body.user.id, user.id)
   })
 
   test('it should update the password of the user', async (assert) => {
-    const user = await UserFactory.create()
     const password = 'test'
 
     const { body } = await supertest(BASE_URL)
@@ -137,13 +137,14 @@ test.group('User', (group) => {
 
   group.before(async () => {
     const plainPassword = 'test'
-    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+    const newUser = await UserFactory.merge({ password: plainPassword }).create()
     const { body } = await supertest(BASE_URL)
       .post('/sessions')
-      .send({ email, password: plainPassword })
+      .send({ email: newUser.email, password: plainPassword })
       .expect(201)
 
     token = body.token.token
+    user = newUser
   })
 
   group.beforeEach(async () => {
