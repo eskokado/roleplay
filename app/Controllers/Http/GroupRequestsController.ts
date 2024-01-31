2f445f5b081bb173656994d87611cd5a3a418cd1
eskokado <eskokado@gmail.com>
2024-01-31 10:55:47 -0300

    feat: it should return 409 when group request already exists

--- app/Controllers/Http/GroupRequestsController.ts
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -1,3 +1,4 @@
+import BadRequest from 'App/Exceptions/BadRequestException'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import GroupRequest from 'App/Models/GroupRequest'
 
@@ -5,6 +6,13 @@ export default class GroupRequestsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const groupId = request.param('groupId') as number
     const userId = auth.user!.id
+
+    const existingGroupRequest = await GroupRequest.query()
+      .where('groupId', groupId)
+      .andWhere('userId', userId)
+      .first()
+
+    if (existingGroupRequest) throw new BadRequest('group request already exists', 409)
 
     const groupRequest = await GroupRequest.create({ groupId, userId })
     await groupRequest.refresh()
