df669c69f0a04b66afbab37545b0be9622b68e8f
eskokado <eskokado@gmail.com>
2024-02-06 06:52:57 -0300

    feat: it should return all groups when no query is provided to list groups

--- start/routes.ts
+++ start/routes.ts
@@ -33,12 +33,14 @@ Route.post('/reset-password', 'PasswordsController.resetPassword')
 Route.post('/sessions', 'SessionsController.store')
 Route.delete('/sessions', 'SessionsController.destroy')
 
+Route.get('/groups', 'GroupsController.index').middleware('auth')
 Route.post('/groups', 'GroupsController.store').middleware('auth')
 Route.patch('/groups/:id', 'GroupsController.update').middleware('auth')
 Route.delete('/groups/:id', 'GroupsController.destroy').middleware('auth')
 Route.delete('/groups/:groupId/players/:playerId', 'GroupsController.removePlayer').middleware(
   'auth'
 )
+
 Route.post('/groups/:groupId/requests', 'GroupRequestsController.store').middleware('auth')
 Route.get('/groups/:groupId/requests', 'GroupRequestsController.index').middleware('auth')
 Route.post(
